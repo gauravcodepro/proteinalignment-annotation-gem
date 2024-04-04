c4984d26f669f48fe8ee7707ecc79c1176d7ec9a
Gaurav Sablok <sablokg@gmail.com>
2024-04-04 13:23:19 +0200

    Update proteinalignment.rb

--- proteinalignment.rb
+++ proteinalignment.rb
@@ -2,48 +2,96 @@
 # Author Gaurav Sablok
 # Universitat Potsdam
 # Date 2024-4-4
-=BEGIN
-    a ruby gem for all the alignment and the pattern
-    extracttion from the alignments. It plots also the
-    alignment length and also a three dimensional plot
-    using the mRNA, cds, stop positions.
-=END
+   # a ruby gem for all the alignment and the pattern
+   # extracttion from the alignments. It plots also the
+   # alignment length and also a three dimensional plot
+   # using the mRNA, cds, stop positions. complete support
+   # for the protein annotations and the alignment of the genome
+   # to the proteins and generation of the hints for the genome
+   # annotations.
 
 gem install gr-plot
 require 'gr/plot'
 
-class proteinAlignment 
-def initialize(alignmentfile, writemodifiedbuffer)
-  @alignment = alignmentfile
-  @modifiedbuffer = writemodifiedbuffer
-  readfile = File.open(@alignment)
-  writefile = File.new(writemodifiedbuffer, "w")
-  writefile.puts(readfile[2..readfile.length])
-  writefile.close
-end
 
-def getcorrdinates(start, stop, type)
- if start == "mRNA"
+class ProteinAlignment
+
+
+ def initialize(alignmentfile, writemodifiedbuffer)
+   @alignment = alignmentfile
+   @modifiedbuffer = writemodifiedbuffer
+   readfile = File.open(@alignment).readlines
+   writefile = File.new(writemodifiedbuffer, "w")
+   writefile.puts(readfile[2..readfile.length])
+   writefile.close
+ end 
+ 
+ def getcorrdinates(inputfile, outputfile, type)
+  if type == "mRNA"
     columnread = []
     start_cor = []
     end_cor = []
+    readfile = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
     fileopen = File.open(writefile)
     fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
     fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
     fileopen.each { | iter | end_cor.push(iter.strip().split()[4])}
     mRNA = []
     for i in 0..columnread.length
-       mRNA.push(columnread[i],start_cor[i],end_cor[i] if columnread[i]=="mRNA")
+       mRNA.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="mRNA"
     end
     return mRNA
   end
+  if type == "CDS"
+    columnread = []
+    start_cor = []
+    end_cor = []
+    readfile = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
+    fileopen = File.open(writefile)
+    fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
+    fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
+    fileopen.each { | iter | end_cor.push(iter.strip().split()[4])}
+    cds = []
+    for i in 0..columnread.length
+       cds.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="CDS"
+    end
+    return mRNA
   end
+  if type == "stop_codon"
+    columnread = []
+    start_cor = []
+    end_cor = []
+    readfile = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
+    fileopen = File.open(writefile)
+    fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
+    fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
+    fileopen.each { | iter | end_cor.push(iter.strip().split()[4])}
+    stop_codon = []
+    for i in 0..columnread.length
+       stop_codon.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="stop_codon"
+    end
+    return mRNA
+  end
+end
 
-def plotmRNAs()
+ def plotmRNAs(inputfile, outputfile)
     if start == "mRNA"
     columnread = []
     start_cor = []
     end_cor = []
+    fileopen = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
     fileopen = File.open(writefile)
     fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
     fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
@@ -53,25 +101,29 @@ def plotmRNAs()
     mRNA_end = []
     mRNA_difference = []
     for i in 0..columnread.length
-       mRNA.push(columnread[i],start_cor[i],end_cor[i] if columnread[i]=="mRNA")
+       mRNA.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="mRNA"
     end
     for i in 0..columnread.length
-       mRNA_start.push(start_cor[i] if columnread[i]=="mRNA")
+       mRNA_start.push(start_cor[i].to_i) if columnread[i]=="mRNA"
     end
     for i in 0..columnread.length
-       mRNA_end.push(end_cor[i] if columnread[i]=="mRNA")
+       mRNA_end.push(end_cor[i].to_i) if columnread[i]=="mRNA"
     end
     for i in 0..mRNA_start.length
-        mRNA_difference.push(mRNA_end[i].to_i-mRNA_start[i].to_i)
+        mRNA_difference.push(mRNA_end[i].to_i - mRNA_start[i].to_i)
     end
-    return GR.barplot(mRNA_difference)
-end
+    GR.barplot(mRNA_difference)
+ end
 
-def plotCDS()
+ def plotCDS(inputfile, outputfile)
     if start == "CDS"
     columnread = []
     start_cor = []
     end_cor = []
+    fileopen = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
     fileopen = File.open(writefile)
     fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
     fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
@@ -81,49 +133,53 @@ def plotCDS()
     cds_end = []
     cds_difference = []
     for i in 0..columnread.length
-       cds.push(columnread[i],start_cor[i],end_cor[i] if columnread[i]=="CDS")
+       cds.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="CDS"
     end
     for i in 0..columnread.length
-       cds_start.push(start_cor[i] if columnread[i]=="CDS")
+       cds_start.push(start_cor[i].to_i) if columnread[i]=="CDS"
     end
     for i in 0..columnread.length
-       cds_end.push(end_cor[i] if columnread[i]=="CDS")
+       cds_end.push(end_cor[i].to_i) if columnread[i]=="CDS"
     end
     for i in 0..mRNA_start.length
         cds_difference.push(cds_end[i].to_i - cds_start[i].to_i)
     end
     return GR.barplot(cds_difference)
-end
+ end
 
-def plotstop()
+ def plotstop(inputfile, outputfile)
     if start == "stop_codon"
     columnread = []
     start_cor = []
     end_cor = []
+    fileopen = File.open(inputfile).readlines
+    writefile = File.new(outputfile, "w")
+    writefile.puts(readfile[2..readfile.length])
+    writefile.close
     fileopen = File.open(writefile)
     fileopen.each { | iter | columnread.push(iter.strip().split()[2]) }
     fileopen.each { | iter | start_cor.push(iter.strip().split()[3]) }
-    fileopen.each { | iter | end_cor.push(iter.strip().split()[4])}
+    fileopen.each {   iter | end_cor.push(iter.strip().split()[4])}
     stop = []
     stop_start = []
     stop_end = []
     stop_difference = []
     for i in 0..columnread.length
-       stop.push(columnread[i],start_cor[i],end_cor[i] if columnread[i]=="CDS")
+       stop.push([columnread[i].to_s,start_cor[i].to_i,end_cor[i].to_i]) if columnread[i]=="stop_codon"
     end
     for i in 0..columnread.length
-       stop_start.push(start_cor[i] if columnread[i]=="CDS")
+       stop_start.push(start_cor[i].to_i) if columnread[i]=="stop_codon"
     end
     for i in 0..columnread.length
-       stop_end.push(end_cor[i] if columnread[i]=="CDS")
+       stop_end.push(end_cor[i].to_i) if columnread[i]=="stop_codon"
     end
     for i in 0..mRNA_start.length
        stop_difference.push(stop_end[i].to_i - stop_start[i].to_i)
     end
-    return GR.barplot(stop_difference)
-end
+    GR.barplot(stop_difference)
+ end
 
-def 3D()
-   return GR.scatter3(mRNA_differences,cds_differences,stop_codon_differences)
-end
-end
+  def 3D()
+    GR.scatter3(mRNA_differences,cds_differences. stop_codon_differences)
+  end
+ end
